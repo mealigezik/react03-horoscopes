e0fd2942bde6203cc958ef754f039cb822ee39f9
Ali Gezik <bilgepanter@gmail.com>
2023-03-23 18:14:19 +0100

    complate cards

--- src/components/Main/Card.jsx
+++ src/components/Main/Card.jsx
@@ -1,10 +1,23 @@
 // import
 
 const Card = (data) => {
-    console.log("ne gelir", data);
-    return(
-        <h1>Card</h1>
-    )
-}
+  console.log("ne gelir", data);
+  return (
+    <div className="cards">
+      <div className="title">
+        <h1>{data.title}</h1>
+      </div>
+      <div className="date">
+        <h1>{data.date}</h1>
+      </div>
+      <img src={data.image} alt={data.title} />
+      <div className="card-over">
+        <p>
+            {data.desc}
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
