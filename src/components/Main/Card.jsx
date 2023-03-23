6dd95d1c09251ae95a9ee2989bb32650d8ab83dd
Ali Gezik <bilgepanter@gmail.com>
2023-03-23 17:33:57 +0100

    connect components

---
+++ src/components/Main/Card.jsx
@@ -0,0 +1,10 @@
+// import
+
+const Card = (data) => {
+    console.log("ne gelir", data);
+    return(
+        <h1>Card</h1>
+    )
+}
+
+export default Card;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,16 @@
 import "./Main.scss";
+import { data } from "../../helpers/data";
+import Card from "./Card";
 
 const Main = () => {
+    // console.log(data);
     return (
         <div className="card-container">
-            <h1>Card</h1>
+            {data.map((item) => <Card 
+                {...item}
+            />)}
         </div>
-    )
-}
+    );
+};
 
 export default Main;
