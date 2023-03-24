4639bf79dc3ae1af811b7aceb5c8a2e09d28013a
Ali Gezik <bilgepanter@gmail.com>
2023-03-24 23:47:31 +0100

    q

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
             </div>
             <div className="links">
                 <a href="#horoscope">Horoscope</a>
-                <a href="#daily">Daily</a>
+                <a href="#daily">Daily.</a>
                 <a href="#tarot">Tarot</a>
                 <a href="#article">Article</a>
                 <a href="#contact">Contact</a>
